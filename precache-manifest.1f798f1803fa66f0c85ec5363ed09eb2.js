5f377d9c0697a300d0fe1315ce27bb435dc6ef4d
Michal <michal@ja.com>
2020-07-02 21:46:40 +0200

    Updates

---
+++ precache-manifest.1f798f1803fa66f0c85ec5363ed09eb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f65bd033b130f8149d1c90fc46e67b3",
+    "url": "/testweb/index.html"
+  },
+  {
+    "revision": "928f46eeff2d8e22b5cc",
+    "url": "/testweb/static/css/main.c966226e.chunk.css"
+  },
+  {
+    "revision": "2328c102e360b0a6ee91",
+    "url": "/testweb/static/js/2.295043a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testweb/static/js/2.295043a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928f46eeff2d8e22b5cc",
+    "url": "/testweb/static/js/main.66281e89.chunk.js"
+  },
+  {
+    "revision": "a7c24d1f5f6c76d367e4",
+    "url": "/testweb/static/js/runtime-main.bf629fb4.js"
+  },
+  {
+    "revision": "c22abaa24f623e66ede766a119ab1238",
+    "url": "/testweb/static/media/background.c22abaa2.jpeg"
+  },
+  {
+    "revision": "ad28f07c97d5fa225d8cfd4d4ac8d3f6",
+    "url": "/testweb/static/media/golden_gate.ad28f07c.mp4"
+  }
+]);
